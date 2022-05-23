62707bd943dcba02c331cbbad77af3d2d6041e72
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-23 17:29:14 +0430

    message

---
+++ android/app/src/main/kotlin/com/example/game_/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame with DoubleTapDetector, HasTappables {}
